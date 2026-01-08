5384d46b702d2dc60b4bc9741fe77f920473d1b8
Rudra Pratap Singh <rudrapratap7488@gmail.com>
2026-01-08 09:55:08 +0530

    cors error solve

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,16 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "https://lead-automation-frm0.onrender.com"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 app.use(express.json());
 
 // Route
