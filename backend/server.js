3ce97b967a3b10276ad9509dfe761e30fd8c1b9f
Rudra Pratap Singh <rudrapratap7488@gmail.com>
2026-01-08 10:09:35 +0530

    solve cors

--- backend/server.js
+++ backend/server.js
@@ -14,8 +14,8 @@ app.use(cors({
   origin: function (origin, callback) {
     const allowedOrigins = [
       "http://localhost:3000",
-      "http://localhost:5173",
-      "https://lead-automation-frm0.onrender.com"
+      "http://localhost:5173"
+      
     ];
 
     // allow requests with no origin (Postman, curl)
